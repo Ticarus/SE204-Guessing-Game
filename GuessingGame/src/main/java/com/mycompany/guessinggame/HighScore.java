1c23b4aad241da45a37776fcc6ae6f6d21e7cd8b
Ticarus <101121181+Ticarus@users.noreply.github.com>
2023-03-23 14:53:38 +0300

    SE204-Guessing-Game V1.1.2

--- GuessingGame/src/main/java/com/mycompany/guessinggame/HighScore.java
+++ GuessingGame/src/main/java/com/mycompany/guessinggame/HighScore.java
@@ -25,7 +25,24 @@ public class HighScore {
     }
     
     public void printHighScore(){
-        System.out.println("Guess the number best score: " + this.gtnHighScore + " chances used up.");
-        System.out.println("Guess the alphabet best score: " + this.gtaHighScore + " chances used up.");
+        System.out.println("High Score");
+        System.out.println("----------");
+        
+        if(this.gtnHighScore == 0 && this.gtaHighScore == 0){
+            System.out.println("Guess the number best score: Have not played yet");
+            System.out.println("Guess the alphabet best score: Have not played yet");
+        }
+        else if(this.gtnHighScore > 0 && this.gtaHighScore == 0){
+            System.out.println("Guess the number best score: " + this.gtnHighScore + " chances used up.");
+            System.out.println("Guess the alphabet best score: Have not played yet");
+        }
+        else if(this.gtnHighScore == 0 && this.gtaHighScore > 0){
+            System.out.println("Guess the number best score: Have not played yet");
+            System.out.println("Guess the alphabet best score: " + this.gtaHighScore + " chances used up.");
+        }
+        else{
+            System.out.println("Guess the number best score: " + this.gtnHighScore + " chances used up.");
+            System.out.println("Guess the alphabet best score: " + this.gtaHighScore + " chances used up.");
+        }     
     }
 }
